8b81d19e61c1b04b5f557723e41e4c8a75ba94f5
rahul <sangewarr@gmail.com>
2021-09-19 19:04:47 -0400

    updates gui and text fields

--- src/CPUsimGUI.java
+++ src/CPUsimGUI.java
@@ -88,6 +88,18 @@ public class CPUsimGUI extends JFrame {
         });
 
         //load from file button action
+        loadButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(e.getSource()==loadButton){
+                    JFileChooser loadFile = new JFileChooser();
+                    int resp = loadFile.showOpenDialog(null);
+                    if (resp == JFileChooser.APPROVE_OPTION){
+                        //forward to input over here...!!!!
+                    }
+                }
+            }
+        });
     }
 
     //Main
