af37987066e87b0acaebec7262c6fa49f2949e4b
yifan Chen <drinket520@gmail/com>
2021-09-19 18:19:55 -0400

    memory

--- src/Memory.java
+++ src/Memory.java
@@ -7,7 +7,38 @@ public class Memory {
         for (int i=0;i<2048;i++){
             memoryArray[i][0]  =i;
             memoryArray[i][1] = "0000,0000,0000,0000";
+
         }
+    }
+
+    @Override
+    public String toString(){
+        String output = new String("");
+        for(int a = 0; a < this.memoryArray.length;a++){
+            output = output + memoryArray[a][0]+":" + memoryArray[a][1] + "\n";
+        }
+        return output;
+
+    }
+    //show used memory as a string output,
+    public String showUsedMemory(){
+        String output = " ";
+        for (int i = 0; i < this.memoryArray.length; i++) {
+            if (this.memoryArray[i][1] != "0000,0000,0000,0000") {
+                output = output + memoryArray[i][0]+":" + memoryArray[i][1] + "\n";
+            }
+            if(output.equals(" ")) {
+                output = "Nothing in memory";
+            }
+
+        }
+        return output;
+    }
+    //for testing
+    public static void main(String[] args){
+
+        System.out.println(new Memory().toString());
+        System.out.println(new Memory().showUsedMemory());
     }
     public void putMem(int i,String value){
         memoryArray[i][1]=value;
